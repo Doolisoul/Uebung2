e769b3529bc9a08e9ed13f4605e6de96b9378e45
Panagiotis <sot20del09@gmail>
2025-10-30 17:00:25 +0100

    fifth commit

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,5 +1,14 @@
 package p2;
 
 public class P2_main {
+	public static void main(String[] args) {
+		int i = 22;
+		boolean b = false;
+		
+		if (i > 10) b = true;
+		
+		System.out.println(b);
+		
+	}
 
 }
